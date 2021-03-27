d15009b684b994201b025a22d8f0532d167ebf3e
TNAR5 <tnars@tnars.ddns.net>
2021-03-27 01:57:52 -0400

    Added ssh and apt support.

--- linux/lockdown.sh
+++ linux/lockdown.sh
@@ -15,7 +15,13 @@ echo "OS:" `uname -o`
 
 function ssh_lockdown()
 {
-echo "a"
+	if dpkg --get-selections | grep -q "^openssh-server[[:space:]]*install$" >/dev/null;then
+		echo "[+] SSH is installed switching to secure config."
+		cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
+		printf "Port 22\nPermitRootLogin no\nListenAddress 0.0.0.0\nMaxAuthTries 3\nMaxSessions 1\nPubkeyAuthentication yes\nPermitEmptyPasswords no\nUsePAM yes\nPrintMotd yes\nAcceptEnv LANG LC_*\nSubsystem\tsftp\t/usr/lib/openssh/sftp-server" > /etc/ssh/sshd_config
+	else
+		echo "[-] SSH is not installed."
+	fi
 }
 
 function kernel_lockdown()
@@ -37,3 +43,17 @@ function user_lockdown()
 {
 echo "e"
 }
+
+function ask_to_install_updates()
+{
+	read -p "Would you like to install updates? [y/n] " -n 1 -r
+	echo
+	if [[ $REPLY =~ ^[Yy]$ ]]
+	then
+		apt-get update
+		apt-get upgrade
+	fi
+}
+
+ssh_lockdown
+ask_to_install_updates
