dcaff9d9246b8db1a768f9f2ccd96a46d723f955
Declan Oberzan <doberzan42@gmail.com>
2021-03-28 00:41:39 -0500

    Added media file checking

--- linux/lockdown.sh
+++ linux/lockdown.sh
@@ -3,13 +3,13 @@
 # Author: TNAR5
 # Version: 1
 
-CURRENT_USER=$(whoami)
+CURRENT_USER=$(logname)
 RED='\033[0;31m'
 GREEN='\033[1;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m'
 
-function warn()
+function notify()
 {
 	echo -e "$YELLOW[!]$NC $1"
 }
@@ -26,15 +26,16 @@ function success()
 
 
 
-if ! [[ $CURRENT_USER == "root" ]];then 
+if ! [[ $(whoami) == "root" ]];then 
 error "You must execute this script as root."
 exit 1
 fi
 
 echo "Linux lockdown script"
-echo "Author: TNAR5"
-echo "Version: 1"
-echo "OS:" `uname -o`
+echo "Author........: TNAR5"
+echo "Version.......: 1.0"
+echo "OS............: $(uname -o)"
+echo "Executing User: $(logname)"
 
 
 read -p "[?] Have you read the README and the Forensics Questions? [y/n]" -n 1 -r
@@ -42,6 +43,7 @@ echo
 if [[ $REPLY =~ ^[Nn]$ ]]
 then
 	error "Please read the files on the desktop to make sure that the script is not messing with anything essential."
+	exit 1
 fi
 
 
@@ -74,14 +76,14 @@ echo "c"
 function user_lockdown()
 {
 	printf "\nUser Lockdown\n"
-	warn "Starting interactive user lockdown."
-	success "Backup user list ${HOME}/users.txt"
-	getent passwd | grep "home" | cut -d ':' -f 1 > ~/users.txt
+	notify "Starting interactive user lockdown."
+	success "Backup user list /home/$CURRENT_USER/users.txt"
+	getent passwd | grep "home" | cut -d ':' -f 1 > /home/$CURRENT_USER/users.txt
 	users=($(getent passwd | grep "home" | cut -d ':' -f 1))
 	success "Found "${#users[@]}" users."
 	for u in "${users[@]}"
 	do
-		read -p "[?] Is user "$u" authorized to be on the system? [y/n] " -n 1 -r
+		read -p "[?] Is user ${u} authorized to be on the system? [y/n] " -n 1 -r
 		echo
 		if [[ $REPLY =~ ^[Nn]$ ]]
 		then
@@ -110,9 +112,9 @@ function user_lockdown()
 				groups $u | grep "sudo" > /dev/null
 				if [ $? -eq 0 ];
 				then 
-					warn "User was an Administrator."
+					notify "User was an Administrator."
 					#deluser $u sudo
-					success "Removed "$u" from sudo group."
+					success "Removed ${u} from sudo group."
 				else
 					success "User is not an Administrator - no change."
 				fi			
@@ -182,7 +184,7 @@ function ask_to_install_updates()
 	fi
 }
 
-function check_files()
+function check_configs()
 {
 	read -p "[?] Would you like to check random system config files? [y/n] " -n 1 -r
 		echo
@@ -203,74 +205,100 @@ function check_bad_programs()
 {
 	echo -e "\nChecking for 'bad' programs."
 	if dpkg --get-selections | grep -q "^nmap[[:space:]]*install$" >/dev/null;then
-		warn "Nmap is installed, removing."
+		notify "Nmap is installed, removing."
 		#apt-get purge nmap
 	fi
 	if dpkg --get-selections | grep -q "^john[[:space:]]*install$" >/dev/null;then
-		warn "John is installed, removing."
+		notify "John is installed, removing."
 		#apt-get purge john
 	fi
 	if dpkg --get-selections | grep -q "^rainbowcrack[[:space:]]*install$" >/dev/null;then
-		warn "rainbowcrack is installed, removing."
+		notify "rainbowcrack is installed, removing."
 		#apt-get purge rainbowcrack
 	fi
 	if dpkg --get-selections | grep -q "^ophcrack[[:space:]]*install$" >/dev/null;then
-		warn "Ophcrack is installed, removing."
+		notify "Ophcrack is installed, removing."
 		#apt-get purge ophcrack
 	fi
 	if dpkg --get-selections | grep -q "^nc[[:space:]]*install$" >/dev/null;then
-		warn "Nc is installed, removing."
+		notify "Nc is installed, removing."
 		#apt-get purge nc
 	fi
 	if dpkg --get-selections | grep -q "^netcat[[:space:]]*install$" >/dev/null;then
-		warn "Netcat is installed, removing."
+		notify "Netcat is installed, removing."
 		#apt-get purge netcat
 	fi
 	if dpkg --get-selections | grep -q "^hashcat[[:space:]]*install$" >/dev/null;then
-		warn "Hashcat is installed, removing."
+		notify "Hashcat is installed, removing."
 		#apt-get purge hashcat
 	fi
 	#apt-get purge netcat*
 
 	if dpkg --get-selections | grep -q "^samba[[:space:]]*install$" >/dev/null;then
-		warn "Samba is installed, make sure this is a required service."
+		notify "Samba is installed, make sure this is a required service."
 	fi
 	if dpkg --get-selections | grep -q "^bind9[[:space:]]*install$" >/dev/null;then
-		warn "Bind9 is installed, make sure this is a required service."
+		notify "Bind9 is installed, make sure this is a required service."
 	fi
 	if dpkg --get-selections | grep -q "^vsftpd[[:space:]]*install$" >/dev/null;then
-		warn "Vsftpd is installed, make sure this is a required service."
+		notify "Vsftpd is installed, make sure this is a required service."
 	fi
 	if dpkg --get-selections | grep -q "^apache2[[:space:]]*install$" >/dev/null;then
-		warn "Apache2 is installed, make sure this is a required service."
+		notify "Apache2 is installed, make sure this is a required service."
 	fi
 	if dpkg --get-selections | grep -q "^nginx[[:space:]]*install$" >/dev/null;then
-		warn "Nginx is installed, make sure this is a required service."
+		notify "Nginx is installed, make sure this is a required service."
 	fi
 	if dpkg --get-selections | grep -q "^telnet[[:space:]]*install$" >/dev/null;then
-		warn "Telnet is installed, make sure this is a required service."
+		notify "Telnet is installed, make sure this is a required service."
 	fi
 	success "Displaying other active services:"
 	service --status-all | grep '+'
 }
 
+function find_media()
+{
+	notify "Checking for media files on the system and outputing to file."
+	success "Checking txt files."
+	find . -type f -name "*.txt" 
+	success "Checking mp4 files."
+	find . -type f -name "*.mp4"
+	success "Checking mp3 files."
+	find . -type f -name "*.mp3" 
+	success "Checking ogg files."
+	find . -type f -name "*.ogg"
+	success "Checking wav files."
+	find . -type f -name "*.wav" 
+	success "Checking png files."
+	find . -type f -name "*.png"
+	success "Checking jpg files."
+	find . -type f -name "*.jpg"
+	find . -type f -name "*.jpeg" 
+	success "Checking gif files."
+	find . -type f -name "*.gif"
+	success "Checking mov files."
+	find . -type f -name "*.mov"
+	
+	
+}
 
 ssh_lockdown
 enable_ufw
 enable_av
 kernel_lockdown
 user_lockdown
-check_files
+check_configs
 #ask_to_install_updates
 check_bad_programs
+find_media
 
 echo -e "\nThings left to do:"
-warn "Update kernel"
-warn "Pam cracklib password requirements"
-warn "Discover rootkits"
-warn "Check file permissions"
-warn "Check for media files, mp4, mp3, ogg, wav, png, jpg, jpeg, gif, mov, txt, "
-warn "Win"
+notify "Update kernel"
+notify "Pam cracklib password requirements"
+notify "Discover rootkits"
+notify "Check file permissions"
+notify "Check for media files, mp4, mp3, ogg, wav, png, jpg, jpeg, gif, mov, txt, "
+notify "Win"
 
 success "Script finished exiting."
 exit 0
